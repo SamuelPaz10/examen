bc56d10a4e250880c5f0a48bd3c97a34ebca604d
SamuelPaz10 <111510648+SamuelPaz10@users.noreply.github.com>
2024-08-02 19:05:30 -0600

    Obtener y agregar clientes

--- src/main/java/hn/unah/lenguajes/_0/examen/controladores/ClienteControlador.java
+++ src/main/java/hn/unah/lenguajes/_0/examen/controladores/ClienteControlador.java
@@ -1,5 +1,33 @@
 package hn.unah.lenguajes._0.examen.controladores;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import hn.unah.lenguajes._0.examen.modelos.Cliente;
+import hn.unah.lenguajes._0.examen.servicios.ClienteServicios;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/api/cliente")
 public class ClienteControlador {
-    
+
+    @Autowired
+    private ClienteServicios clienteServicios;
+
+    @GetMapping("/obtener/todos")
+    public List<Cliente> obtenerTodos() {
+        return this.clienteServicios.obtenerTodos();
+    }
+
+    @PostMapping("/crear/nuevo")
+    public Cliente crearCliente(@RequestBody Cliente nuevoCliente) {
+        return this.clienteServicios.crearCliente(nuevoCliente);
+    }
+
 }
--- src/main/java/hn/unah/lenguajes/_0/examen/servicios/ClienteServicios.java
+++ src/main/java/hn/unah/lenguajes/_0/examen/servicios/ClienteServicios.java
@@ -1,8 +1,39 @@
 package hn.unah.lenguajes._0.examen.servicios;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import hn.unah.lenguajes._0.examen.modelos.Cliente;
+import hn.unah.lenguajes._0.examen.modelos.Prestamos;
+import hn.unah.lenguajes._0.examen.repositorios.ClienteRepositorio;
+import hn.unah.lenguajes._0.examen.repositorios.PrestamosRepositorio;
 
 @Service
 public class ClienteServicios {
 
+    @Autowired
+    private ClienteRepositorio clienteRepositorio;
+
+    @Autowired
+    private PrestamosRepositorio prestamosRepositorio;
+
+    public List<Cliente> obtenerTodos() {
+        return this.clienteRepositorio.findAll();
+    }
+
+    public Cliente crearCliente(Cliente nuevoCliente) {
+        if (this.clienteRepositorio.existsById(nuevoCliente.getDni())) {
+            return null;
+        }
+
+        List<Prestamos> prestamos = nuevoCliente.getPrestamos();
+        if (prestamos != null) {
+            for (Prestamos prestamo : prestamos) {
+                prestamo.setCliente(nuevoCliente);
+            }
+        }
+        return this.clienteRepositorio.save(nuevoCliente);
+    }
 }
